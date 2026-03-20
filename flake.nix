2849900ec96eec6a717911748278c324e71b2209
DNKYr <dnkyr2007@gmail.com>
2026-03-19 22:43:57 -0400

    ADD(flake): pnpm support using flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "astro-koharu blog development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            pnpm
+          ];
+        };
+      }
+    );
+}
